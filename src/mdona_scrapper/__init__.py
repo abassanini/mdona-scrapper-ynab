433c27f2f73fa3b4445d78d96abe7b44a8f29ead
Aldo Bassanini <aldoxavier.bassanini@servexternos.repsol.com>
2025-06-03 11:31:48 +0200

    feat(common): multiple adjustmens
    feat: changing to loguru module instead of logging module
    refactor: use of invoice_scrapper common class
    feat: regex adjustments to support new invoice format
    feat: new methods to support new invoice format downloaded with ticket id

--- src/mdona_scrapper/__init__.py
+++ src/mdona_scrapper/__init__.py
@@ -1,58 +1,23 @@
 import re
-from dataclasses import dataclass
 from datetime import datetime
-from io import BufferedReader, BytesIO
-from pathlib import Path
 from typing import List
 
-import pandas as pd
-from pypdf import PdfReader
-from pypdf.errors import PyPdfError
+from loguru import logger
+
+from invoice_scrapper import InvoiceScrapper
+from models import Invoice, Product
 
 
-@dataclass
-class Product:
-    name: str
-    total_price: float
-    unit: str
-    quantity: float
-    unit_price: float
-
-
-@dataclass
-class MercadonaInvoice:
-    products: List[Product]
-    order_number: int
-    invoice_number: str
-    payment_date: datetime
-    total: float = 0.0
-
-    @property
-    def dataframe(self):
-        return pd.DataFrame(
-            (
-                {
-                    "name": product.name,
-                    "total_price": product.total_price,
-                    "unit": product.unit,
-                    "quantity": product.quantity,
-                    "unit_price": product.unit_price,
-                    "order_number": self.order_number,
-                    "invoice_number": self.invoice_number,
-                    "payment_date": self.payment_date,
-                    "invoice_total": self.total,
-                }
-                for product in self.products
-            )
-        )
-
-
-class MercadonaScrapper:
-    ORDER_NUMBER_RE = re.compile(r"(?:Pedido Nº|OP):\s+([0-9]+)")
-    INVOICE_NUMBER_RE = re.compile(r"Factura \w+:\s+([0-9\- ]+)\n", re.IGNORECASE)
-    TOTAL_INVOICE_RE = re.compile(r"TOTAL.*€.*?(\d+[.,]\d+)", re.IGNORECASE)
+class MercadonaScrapper(InvoiceScrapper):
+    INVOICE_NUMBER_RE = re.compile(
+        r"^Factura \w+:\s*([0-9\- ]+)", re.IGNORECASE | re.MULTILINE
+    )
+    TOTAL_INVOICE_RE = re.compile(
+        r"TOTAL.*€.*?(\d+[.,]\d+)$", re.IGNORECASE | re.MULTILINE
+    )
     PAYMENT_DATE_RE = re.compile(
-        r"(?:Cobrado el )?([0-9]+)/([0-9]+)/([0-9]+)(?: a las)?\s+([0-9]+):([0-9]+)"
+        r"(?:Fecha factura simplificada\s*:\s*|^)([0-9]+)/([0-9]+)/([0-9]+)",
+        re.MULTILINE,
     )
 
     SPECIAL_PRODUCT_RE = re.compile(
@@ -61,6 +26,16 @@ class MercadonaScrapper:
 
     NORMAL_PRODUCT_RE = re.compile(r"^([1-9]\d*)\s+(.+)\s(\d+[.,]\d+)$", re.MULTILINE)
     UNIT_PRODUCT_PRICE_RE = re.compile(r"(.*)\s(\d+[,.]\d{2})$")
+
+    UNITARY_INVOICE_PRODUCT_RE = re.compile(
+        r"^(.+)\s+1\s+(?:\d+[.,]\d+)\s+(?:\d+[.,]\d+)\s+(?:\d+%)\s+(?:\d+[.,]\d+)\s+(\d+[.,]\d+)$",
+        re.MULTILINE,
+    )
+
+    MULTIPLE_INVOICE_PRODUCT_RE = re.compile(
+        r"^(.+)\s+([2-9]|\d{2,})\s+(?:\d+[.,]\d+)\s+(?:\d+[.,]\d+)\s+(?:\d+%)\s+(?:\d+[.,]\d+)\s+(\d+[.,]\d+)$",
+        re.MULTILINE,
+    )
 
     @classmethod
     def _get_special_products(cls, text):
@@ -107,8 +82,9 @@ class MercadonaScrapper:
                     else None
                 )
             except ValueError as e:
-                print(f"Probably regex missmatch in {name=}.  Error: {e}")
-                exit(1)
+                raise SystemExit(
+                    logger.error(f"Probably regex missmatch in {name=}.  Error: {e}")
+                )
 
         unit_price = total_price / quantity if quantity != 0 else 0
         unit = f"({quantity} x {unit.replace(',', '.').strip()}€)" if unit else unit
@@ -130,43 +106,85 @@ class MercadonaScrapper:
         ]
 
     @classmethod
-    def _get_products(cls, text) -> List[Product]:
-        return cls._get_special_products(text) + cls._get_normal_products(text)
+    def _get_unitary_invoice_products(cls, text) -> List[Product]:
+        return [
+            Product(
+                name=name.strip(),
+                quantity=1,
+                unit="",
+                total_price=(t := round(float(total_price.replace(",", ".")), 2)),
+                unit_price=t,
+            )
+            for (
+                name,
+                total_price,
+            ) in cls.UNITARY_INVOICE_PRODUCT_RE.findall(text)
+        ]
 
     @classmethod
-    def _get_order_number(cls, text) -> int:
-        return int(cls.ORDER_NUMBER_RE.search(text).group(1))
+    def _get_multiple_invoice_products(cls, text) -> List[Product]:
+        return [
+            Product(
+                name=name.strip(),
+                quantity=(q := int(quantity)),
+                total_price=(t := round(float(total_price.replace(",", ".")), 2)),
+                unit_price=(u := round(t / q, 2) if q != 0 else 0),
+                unit=f"({q} x {u}€)",
+            )
+            for (
+                name,
+                quantity,
+                total_price,
+            ) in cls.MULTIPLE_INVOICE_PRODUCT_RE.findall(text)
+        ]
+
+    @classmethod
+    def _get_invoice_products(cls, text) -> List[Product]:
+        return cls._get_unitary_invoice_products(
+            text
+        ) + cls._get_multiple_invoice_products(text)
+
+    @classmethod
+    def _get_products(cls, text) -> List[Product]:
+        return (
+            cls._get_special_products(text)
+            + cls._get_normal_products(text)
+            + cls._get_invoice_products(text)
+        )
 
     @classmethod
     def _get_invoice_number(cls, text) -> str:
-        return cls.INVOICE_NUMBER_RE.search(text).group(1)
+        if (match := cls.INVOICE_NUMBER_RE.search(text)) is None:
+            raise SystemExit(
+                logger.error("Regex INVOICE_NUMBER or quality image error.")
+            )
+        else:
+            return match.group(1)
 
     @classmethod
     def _get_payment_date(cls, text) -> datetime:
-        day, month, year, hour, minute = map(
-            int, cls.PAYMENT_DATE_RE.search(text).groups()
-        )
-        return datetime(year, month, day, hour, minute)
+
+        if (match := cls.PAYMENT_DATE_RE.search(text)) is None:
+            raise SystemExit(logger.error("Regex PAYMENT_DATE or quality image error."))
+        else:
+            day, month, year = map(int, match.groups())
+            return datetime(year, month, day)
 
     @classmethod
     def _get_invoice_total(cls, text) -> float:
-        return float(cls.TOTAL_INVOICE_RE.search(text).group(1).replace(",", "."))
+        if (match := cls.TOTAL_INVOICE_RE.search(text)) is None:
+            raise SystemExit(
+                logger.error("Regex TOTAL_INVOICE or quality image error.")
+            )
+        else:
+            return float(match.group(1).replace(",", "."))
 
     @classmethod
-    def get_invoice(
-        cls, path_or_fp: str | Path | BufferedReader | BytesIO
-    ) -> MercadonaInvoice:
-        try:
-            with PdfReader(path_or_fp) as pdf:
-                text = "\n".join([page.extract_text() for page in pdf.pages])
-        except PyPdfError:
-            raise SystemExit(f"Error reading PDF file: {path_or_fp}")
-        except FileNotFoundError:
-            raise SystemExit(f"File not found: {path_or_fp}")
+    def get_invoice(cls, text: str) -> Invoice:
 
-        return MercadonaInvoice(
+        return Invoice(
+            supermarket=cls.supermarket,
             products=cls._get_products(text),
-            order_number=cls._get_order_number(text),
             invoice_number=cls._get_invoice_number(text),
             payment_date=cls._get_payment_date(text),
             total=cls._get_invoice_total(text),
