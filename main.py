5a47d5df369c90bb628b6a3582af58f75f0b9cbf
Aldo Bassanini <aldoxavier.bassanini@servexternos.repsol.com>
2025-06-03 11:34:49 +0200

    feat(common): multiple adjustmens
    feat: changing to loguru module instead of logging module
    refactor: use of invoice_scrapper common class
    refactor: use of util functions
    refactor: supermarket argument not needed anymore.  Now the supermarket
    is readed from the invoice file
    feat: debug arguments in lower
    fix: memo uses invoice and order number

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
-import logging
 import os
 
+from loguru import logger
 from ynab.api.transactions_api import TransactionsApi
 from ynab.api_client import ApiClient
 from ynab.configuration import Configuration
@@ -10,8 +10,8 @@ from ynab.models.new_transaction import NewTransaction
 from ynab.models.post_transactions_wrapper import PostTransactionsWrapper
 from ynab.models.save_sub_transaction import SaveSubTransaction
 
-from consum_scrapper import ConsumScrapper
-from mdona_scrapper import MercadonaScrapper
+from invoice_scrapper import InvoiceScrapper
+from utils import setup_logging
 
 parser = argparse.ArgumentParser(
     description="Extract text from PDF or image bills and send it to YNAB."
@@ -32,18 +32,12 @@ parser.add_argument(
     required=True,
 )
 parser.add_argument(
-    "-s",
-    "--supermarket",
-    type=str,
-    help="Mercadona (m), Consum (c)",
-    required=True,
-)
-
-parser.add_argument(
     "-d",
     "--debug",
-    help="Debug mode: INFO, DEBUG",
-    choices=["INFO", "DEBUG"],
+    help="Debug mode: info, debug, trace.  Default is 'info'.",
+    choices=["info", "debug", "trace"],
+    default="info",
+    type=str.lower,
 )
 
 parser.add_argument(
@@ -56,46 +50,28 @@ parser.add_argument(
 args = parser.parse_args()
 invoice_file: str = args.invoice_file
 ynab_access_token: str = args.token
-supermarket: str = args.supermarket
+setup_logging(args.debug)
 
-if args.debug == "DEBUG":
-    logging.basicConfig(
-        format="%(asctime)s;%(levelname)s: %(message)s", level=logging.DEBUG
-    )
-    logging.debug("Verbose output.")
-elif args.debug == "INFO":
-    logging.basicConfig(
-        format="%(asctime)s;%(levelname)s: %(message)s", level=logging.INFO
-    )
-else:
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.WARNING)
+invoice = InvoiceScrapper.get_invoice(invoice_file)
 
 try:
-    budget_id = os.environ["BUDGET_ID"]  # Spain
-    account_id = os.environ["ACCOUNT_ID"]  # Wise
-    category_id = os.environ["CATEGORY_ID"]  # Split
-    sub_category_id = os.environ["SUB_CATEGORY_ID"]  # Groceries
-    if supermarket == "mercadona" or supermarket == "m":
-        payee_id = os.environ["PAYEE_ID_MERCADONA"]  # Mercadona
-    elif supermarket == "consum" or supermarket == "c":
-        payee_id = os.environ["PAYEE_ID_CONSUM"]  # Consum
+    budget_id = os.environ["BUDGET_ID"]
+    account_id = os.environ["ACCOUNT_ID"]
+    category_id = os.environ["CATEGORY_ID"]
+    sub_category_id = os.environ["SUB_CATEGORY_ID"]
+    if invoice.supermarket == "Consum":
+        payee_id = os.environ["PAYEE_ID_CONSUM"]
+    elif invoice.supermarket == "Mercadona":
+        payee_id = os.environ["PAYEE_ID_MERCADONA"]
 except KeyError as e:
-    logging.error(f"Please set environment variables: {e}")
-    exit(1)
-
-configuration = Configuration(access_token=ynab_access_token)
-
-if supermarket == "mercadona" or supermarket == "m":
-    invoice = MercadonaScrapper.get_invoice(invoice_file)
-elif supermarket == "consum" or supermarket == "c":
-    invoice = ConsumScrapper.get_invoice(invoice_file)
+    raise SystemExit(logger.error(f"Please set environment variables: {e}"))
 
 producs = invoice.products
-order_number = invoice.order_number
 invoice_number = invoice.invoice_number
 payment_date = invoice.payment_date
 total = invoice.total
 
+configuration = Configuration(access_token=ynab_access_token)
 data = PostTransactionsWrapper(
     transaction=NewTransaction(
         account_id=account_id,
@@ -103,7 +79,7 @@ data = PostTransactionsWrapper(
         amount=int(-invoice.total * 1000),
         payee_id=payee_id,
         category_id=category_id,
-        memo=f"YNAB API: Factura={invoice.invoice_number} - Orden={invoice.order_number}",
+        memo=f"YNAB API: Factura={invoice.invoice_number}",
         approved=True,
         subtransactions=[
             SaveSubTransaction(
@@ -117,36 +93,45 @@ data = PostTransactionsWrapper(
 )
 
 
-[logging.info(i) for i in invoice.products]
-logging.info(
-    (
-        f"{total=}, sum_total=",
-        round(sum([i.total_price for i in invoice.products]), 2),
-    )
-)
+[logger.debug(i) for i in invoice.products]
+sum_total = round(sum([i.total_price for i in invoice.products]), 2)
 
-logging.debug(
+if total != sum_total:
+    raise SystemExit(logger.error(f"ERROR: Total Missmatch: {total} != {sum_total}"))
+
+logger.info(
+    f"Invoice number: {invoice.invoice_number}, Payment date: {invoice.payment_date}"
+)
+logger.success(f"{total=}, {sum_total=}")
+
+
+logger.debug(
     (
         "YNAB transation data:",
         f"{data.transaction.var_date=}, {data.transaction.amount=}, {data.transaction.payee_id=}",
         f"{data.transaction.category_id=}, {data.transaction.memo=}",
     )
 )
-[logging.debug(i) for i in data.transaction.subtransactions]
+[logger.debug(i) for i in data.transaction.subtransactions]
 
 if args.dry_run:
-    logging.warning(
-        f"Dry run: not sending data to YNAB - Subtransaction items: {len(data.transaction.subtransactions)}"
+    logger.warning(
+        (
+            f"Dry run: not sending data to YNAB. "
+            f"- Date: {data.transaction.var_date} "
+            f"- Subtransaction items: {len(data.transaction.subtransactions)} "
+            f"- Total: {total}"
+        ),
     )
-    exit(0)
+    raise SystemExit(logger.info("Exiting due to dry run mode."))
 
 # Create a YNAB new transaction
 with ApiClient(configuration) as api_client:
     trx_api = TransactionsApi(api_client)
     try:
         api_response = trx_api.create_transaction(budget_id, data)
-        print(f"YNAB API Response: {api_response.data.transaction_ids}")
+        logger.success(f"YNAB API Response: {api_response.data.transaction_ids}")
     except ApiException as e:
-        logging.error(
+        logger.error(
             "Exception when calling TransactionsApi->create_transaction: %s\n" % e
         )
